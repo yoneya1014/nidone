73737633ebc8712af18ef9c6e2cc66fcafe9014e
MinamiHitoki <mkmokymh@gmail.com>
2018-12-15 16:31:17 +0900

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+//module import
+const server = require('express')()
+const line = require('@line/bot-sdk')
+
+const line_config = {
+    channelAccessToken: process.env.LINE_ACCESS_TOKEN,
+    channelSecret: process.env.LINE_CHANNEL_SECRET
+}
+
+//webServerSetting
+server.listen(process.env.PORT || 3000)
+
+//routerSetting
+server.post('webhook', line.middleware(line_config), (req, res, next) => {
+    res.sendStatus(200)
+    console.log(req.body)
+})
