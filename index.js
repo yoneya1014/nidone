93314f74c680e3663c8d5194544f15c99d131093
MinamiHitoki <mkmokymh@gmail.com>
2018-12-15 16:37:31 +0900

    fix

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-//module import
-const server = require('express')()
-const line = require('@line/bot-sdk')
+// -----------------------------------------------------------------------------
+// モジュールのインポート
+const server = require("express")();
+const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
 
+// -----------------------------------------------------------------------------
+// パラメータ設定
 const line_config = {
-    channelAccessToken: process.env.LINE_ACCESS_TOKEN,
-    channelSecret: process.env.LINE_CHANNEL_SECRET
-}
+    channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
+    channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
+};
 
-//webServerSetting
-server.listen(process.env.PORT || 3000)
+// -----------------------------------------------------------------------------
+// Webサーバー設定
+server.listen(process.env.PORT || 3000);
 
-//routerSetting
-server.post('webhook', line.middleware(line_config), (req, res, next) => {
-    res.sendStatus(200)
-    console.log(req.body)
-})
+
+// -----------------------------------------------------------------------------
+// ルーター設定
+server.post('/webhook', line.middleware(line_config), (req, res, next) => {
+    res.sendStatus(200);
+    console.log(req.body);
+});
