809cd57b592b67f425ace79aba0c59505cf490d1
MinamiHitoki <mkmokymh@gmail.com>
2018-12-15 16:54:38 +0900

    req test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // -----------------------------------------------------------------------------
 // モジュールのインポート
 const server = require("express")();
-const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
+//const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
 
 // -----------------------------------------------------------------------------
 // パラメータ設定
@@ -17,11 +17,11 @@ server.listen(process.env.PORT || 3000);
 
 // -----------------------------------------------------------------------------
 // ルーター設定
-server.post('/webhook', line.middleware(line_config), (req, res, next) => {
-    res.sendStatus(200);
-    console.log(req.body);
-});
+//server.post('/webhook', line.middleware(line_config), (req, res, next) => {
+//    res.sendStatus(200);
+//    console.log(req.body);
+//});
 
-server.get('/', (res, req, next) => {
-    res.send('OK');
+server.get('/', (req, res, next) => {
+    res.send('OK')
 })
