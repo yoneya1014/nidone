12456cec8056bbb35ff698d6d687630fdd5d5528
MinamiHitoki <mkmokymh@gmail.com>
2018-12-16 04:18:42 +0900

    test

--- SQL/PostTimeStamp.js
+++ SQL/PostTimeStamp.js
@@ -26,8 +26,7 @@ class PostTimeStamp {
                 if(err) {
                     console.log(err.message)
                 }
-                    console.log(rows)
-                })
+            })
             connection.end()
             resolve()
         })
