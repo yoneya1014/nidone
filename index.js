2ecc4c282e5cf01b19733a822d199d7b971e9c81
MinamiHitoki <mkmokymh@gmail.com>
2018-12-15 17:03:27 +0900

    プログラムをもとに戻した

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // -----------------------------------------------------------------------------
 // モジュールのインポート
 const server = require("express")();
-//const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
+const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
 
 // -----------------------------------------------------------------------------
 // パラメータ設定
@@ -17,10 +17,10 @@ server.listen(process.env.PORT || 3000);
 
 // -----------------------------------------------------------------------------
 // ルーター設定
-//server.post('/webhook', line.middleware(line_config), (req, res, next) => {
-//    res.sendStatus(200);
-//    console.log(req.body);
-//});
+server.post('/webhook', line.middleware(line_config), (req, res, next) => {
+    res.sendStatus(200);
+    console.log(req.body);
+});
 
 server.get('/', (req, res, next) => {
     res.send('OK')
