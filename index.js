fe523d3917a062b1f56779616643966b1e78822c
MinamiHitoki <mkmokymh@gmail.com>
2018-12-15 17:17:05 +0900

    messageを返すように

--- index.js
+++ index.js
@@ -14,12 +14,35 @@ const line_config = {
 // Webサーバー設定
 server.listen(process.env.PORT || 3000);
 
+//APIコールのためのクライアントインスタンス
+const bot = new line.Client(line_config);
 
 // -----------------------------------------------------------------------------
 // ルーター設定
 server.post('/webhook', line.middleware(line_config), (req, res, next) => {
     res.sendStatus(200);
-    console.log(req.body);
+    
+    //すべてのイベント処理のプロミスを格納する配列
+    let events_processed = [];
+
+    req.body.events.forEach((event) => {
+        //この処理の対象をイベントタイプがメッセージかるテキストタイプで限定
+        if (event.type == "message" && event.message.type == "text") {
+            if (event.message.text == "こんにちは") {
+                //replyMessage()で返信し、そのプロミスをevents_processedに追加
+                events_processed.push(bot.replyMessage(event.replyToken, {
+                    type: "text",
+                    text: "これはこれは"
+                }));
+            }
+        }
+    });
+
+    Promise.all(events_processed).then(
+        (response) => {
+            console.log(`${response.length} event(s) processed.`);
+        }
+    );
 });
 
 server.get('/', (req, res, next) => {
