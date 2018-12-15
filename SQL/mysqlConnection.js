0b485ae8e559361e4ed797278e15d5087e0d9c00
MinamiHitoki <mkmokymh@gmail.com>
2018-12-16 00:48:51 +0900

    add SqlConnectionDB

---
+++ SQL/mysqlConnection.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql')
+const dbConfig = {
+    host: 'nidone-db.cfg81woz8dyx.us-east-2.rds.amazonaws.com',
+    user: 'yoneya1014',
+    password: 'nidone_db_password',
+    port: 3306,
+    database: 'nidone_db'
+}
+
+const connection = mysql.createConnection(dbConfig)
+
+module.exports = connection
