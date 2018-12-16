f226fd29065599340db2916f1e73fa4c2bea476b
MinamiHitoki <mkmokymh@gmail.com>
2018-12-16 11:36:50 +0900

    タイムが設定されているか確認

--- Data/Emotion.js
+++ Data/Emotion.js
@@ -14,7 +14,7 @@ class Emotion {
         } else {
             console.log("emotionPoints" ,emotionPoints[userId]["emotion"])
             //判定する処理でemotionの値を変える
-            if (emotionPoints[userId]["emotion"] > 10)
+            if (emotionPoints[userId]["emotion"] > -10)
                 emotionPoints[userId]["emotion"] -= 1
             /*
             if (emotionPoints[userId]["emotion"] < 10)
